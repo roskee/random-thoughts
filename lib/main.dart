0007a855d0c50ef7f02f1aaf33df774dff49df8f
kirubel adamu <kirubeladamu@gmail.com>
2021-07-19 14:08:06 +0300

    added add thought functionality

--- lib/main.dart
+++ lib/main.dart
@@ -17,51 +17,83 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Database _database;
+  GlobalKey<ScaffoldState> _scaffoldKey;
   void initState() {
     super.initState();
-    Database.getInstance().then((value) => setState((){
-      _database = value;
-    }));
+    _scaffoldKey = GlobalKey<ScaffoldState>();
+    Database.getInstance().then((value) => setState(() {
+          _database = value;
+        }));
   }
-  String parseDateTime(DateTime dateTime){
+
+  String parseDateTime(DateTime dateTime) {
     return 'Today - 12:30PM';
   }
+
   Widget build(BuildContext context) => MaterialApp(
         title: 'Random Thoughts',
         home: Scaffold(
+          key: _scaffoldKey,
           appBar: AppBar(title: Text('Random Thoughts')),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.post_add),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: _scaffoldKey.currentContext,
+                  builder: (context) => Card(
+                        child: Container(
+                            child: Stack(
+                                alignment: Alignment.bottomRight,
+                                children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'What are you thinking?',
+                                ),
+                                maxLines: 6,
+                                maxLength: 150,
+                              ),
+                              IconButton(
+                                  icon: Icon(
+                                    Icons.send,
+                                  ),
+                                  onPressed: () {})
+                            ])),
+                      ));
+            },
+          ),
           drawer: Drawer(
             child: Text('hi'),
           ),
-          body: (_database == null)?Center(child:CircularProgressIndicator())
-         :StreamBuilder(
-            stream: _database.getCollection('Thoughts'),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData)
-                return Center(child: CircularProgressIndicator());
-              return ListView.builder(
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  return ThoughtView(snapshot.data.docs[index]);
-                  // ThoughtListItem(
-                  //   snapshot.data.docs[index],
-                  //   IconButton(
-                  //       icon: Icon(Icons.thumb_up),
-                  //       onPressed: () {
-                  //         // update likes count
-                  //        _database.updateDoc(snapshot.data.docs[index],
-                  //        (freshSnapshot)=> {'likes': freshSnapshot['likes'] + 1});
-                  //       }
-                  //     ),
-                  //     (){
-                  //       // details about post
-                  //       Navigator.of(context).push(MaterialPageRoute(builder: (context) => ThoughtView(snapshot.data.docs[index])));
-                  //     }
-                  // );
-                },
-              );
-            },
-          ),
+          body: (_database == null)
+              ? Center(child: CircularProgressIndicator())
+              : StreamBuilder(
+                  stream: _database.getCollection('Thoughts'),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData)
+                      return Center(child: CircularProgressIndicator());
+                    return ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return ThoughtView(snapshot.data.docs[index]);
+                        // ThoughtListItem(
+                        //   snapshot.data.docs[index],
+                        //   IconButton(
+                        //       icon: Icon(Icons.thumb_up),
+                        //       onPressed: () {
+                        //         // update likes count
+                        //        _database.updateDoc(snapshot.data.docs[index],
+                        //        (freshSnapshot)=> {'likes': freshSnapshot['likes'] + 1});
+                        //       }
+                        //     ),
+                        //     (){
+                        //       // details about post
+                        //       Navigator.of(context).push(MaterialPageRoute(builder: (context) => ThoughtView(snapshot.data.docs[index])));
+                        //     }
+                        // );
+                      },
+                    );
+                  },
+                ),
         ),
       );
 }
